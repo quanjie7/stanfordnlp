6bbd6f44885283456eb5f344b47ff4cc9b5b00aa
quanjie <3071388565@qq.com>
2017-08-25 10:01:30 +0800

    创建项目

---
+++ src/main/java/cn/com/stanfordnlp/stanfordCoreNLP/App.java
@@ -0,0 +1,13 @@
+package cn.com.stanfordnlp.stanfordCoreNLP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
